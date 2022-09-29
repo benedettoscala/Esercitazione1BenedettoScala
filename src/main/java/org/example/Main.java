5543cef16d81d853f73217a01b6339108e8ae149
Benedetto <scala.benedetto@outlook.it>
2022-09-29 17:00:43 +0200

    First Commit for software engineering

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import static org.junit.Assert.assertEquals;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String str = "Junit is working fine";
+        assertEquals("Junit is working fine",str);
+
+    }
+}
